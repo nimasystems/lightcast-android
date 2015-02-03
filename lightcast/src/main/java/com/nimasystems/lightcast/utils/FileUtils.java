09632fe0a4b64404385f9bf1346cec2853df221d
Martin Kovachev <miracle@nimasystems.com>
2015-02-03 08:19:37 +0200

    LINT

--- lightcast/src/main/java/com/nimasystems/lightcast/utils/FileUtils.java
+++ lightcast/src/main/java/com/nimasystems/lightcast/utils/FileUtils.java
@@ -5,7 +5,6 @@ import android.webkit.MimeTypeMap;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
