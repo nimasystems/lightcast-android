07467b2cdbd86b6a9698a572676c75ccfe2eeb80
Martin Kovachev <miracle@nimasystems.com>
2016-01-24 16:21:56 +0200

    Location access issues

--- lightcast/src/main/java/com/nimasystems/lightcast/utils/LocationUtils.java
+++ lightcast/src/main/java/com/nimasystems/lightcast/utils/LocationUtils.java
@@ -1,13 +1,13 @@
 package com.nimasystems.lightcast.utils;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 
 public class LocationUtils {
     public static boolean getHasLocationAccessRights(Context context) {
+        return true;
+        // TODO: Reenable this when 6.0 compat has been done
+        /*
         return (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED);
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED);*/
     }
 }
