97cb33583a9f49613334a92b015117c5695b939f
Martin Kovachev <miracle@nimasystems.com>
2016-08-13 20:01:08 +0300

    Added funcs

--- lightcast/src/main/java/com/nimasystems/lightcast/utils/FileUtils.java
+++ lightcast/src/main/java/com/nimasystems/lightcast/utils/FileUtils.java
@@ -2,15 +2,53 @@ package com.nimasystems.lightcast.utils;
 
 import android.webkit.MimeTypeMap;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Random;
 
 public class FileUtils {
+
+    public static void createRandomContentsFile(double fileSize, File file) throws FileNotFoundException, UnsupportedEncodingException {
+
+        Random random = new Random();
+        long start = System.currentTimeMillis();
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8")), false);
+        int counter = 0;
+
+        while (true) {
+            String sep = "";
+            for (int i = 0; i < 100; i++) {
+                int number = random.nextInt(1000) + 1;
+                writer.print(sep);
+                writer.print(number / 1e3);
+                sep = " ";
+            }
+            writer.println();
+            //Check to see if the current size is what we want it to be
+            if (++counter == 500) {
+                System.out.printf("Size: %.3f GB%n", file.length() / 1e9);
+                if (file.length() >= fileSize * 1e9) {
+                    writer.close();
+                    break;
+                } else {
+                    counter = 0;
+                }
+            }
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.printf("Took %.1f seconds to create a file of %.3f GB", time / 1e3, file.length() / 1e9);
+    }
 
     public static boolean deleteDirectoryRecursively(File dir, boolean emptyTopDir) {
 
