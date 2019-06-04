d7d8ae66c2953284d2d22752c07cf8461fe3107e
Martin Kovachev <miracle@nimasystems.com>
2019-06-04 10:03:14 +0300

    Changes

--- lightcast/src/main/java/com/nimasystems/lightcast/network/OperationResponse.java
+++ lightcast/src/main/java/com/nimasystems/lightcast/network/OperationResponse.java
@@ -5,13 +5,19 @@
  * */
 package com.nimasystems.lightcast.network;
 
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
 public class OperationResponse {
     private int code;
     private String message;
-    private Object data;
+    private HashMap<String, Object> data;
     private ApiServerErrorModel serverError;
 
-    public OperationResponse(Object data) {
+    public OperationResponse(HashMap<String, Object> data) {
         this.data = data;
     }
 
@@ -29,11 +35,49 @@ public class OperationResponse {
         return message;
     }
 
-    public Object getData() {
+    public HashMap<String, Object> getData() {
         return data;
     }
 
     public ApiServerErrorModel getServerError() {
         return serverError;
+    }
+
+    public int getInt(@NonNull String key) {
+        return data != null ? (int) data.get(key) : 0;
+    }
+
+    public float getFloat(@NonNull String key) {
+        return data != null ? (float) data.get(key) : 0.0f;
+    }
+
+    public double getDouble(@NonNull String key) {
+        return data != null ? (double) data.get(key) : 0.0f;
+    }
+
+    public List<?> getList(@NonNull String key) {
+        return data != null ? (List<?>) data.get(key) : null;
+    }
+
+    public Date getDate(@NonNull String key) {
+        return data != null ? (Date) data.get(key) : null;
+    }
+
+    public String getString(@NonNull String key) {
+        return data != null ? (String) data.get(key) : "";
+    }
+
+    public boolean getBoolean(@NonNull String key) {
+        return data != null && (boolean) data.get(key);
+    }
+
+    public HashMap<String, Object> getMap(@NonNull String key) {
+        //noinspection unchecked
+        return data != null ? (HashMap<String, Object>) data.get(key) : null;
+    }
+
+    public List<HashMap<String, Object>> getMapList(@NonNull String key) {
+        //noinspection unchecked
+        return data != null ? (List<HashMap<String, Object>>) data.get(key) : null;
     }
 }
