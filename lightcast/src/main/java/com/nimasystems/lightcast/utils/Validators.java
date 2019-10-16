3cc2cb444ae82f990c8e69fc19bb06fc2f828870
Martin Kovachev <miracle@nimasystems.com>
2019-10-16 16:13:33 +0300

    Changes

--- lightcast/src/main/java/com/nimasystems/lightcast/utils/Validators.java
+++ lightcast/src/main/java/com/nimasystems/lightcast/utils/Validators.java
@@ -14,6 +14,15 @@ public class Validators {
         return target != null && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 
+    public static boolean isValidEmail2(String email) {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
     public static boolean isValidUsername(String username) {
         String USERNAME_PATTERN = "^[A-Za-z0-9_]{0,30}$";
         Pattern pattern = Pattern.compile(USERNAME_PATTERN);
