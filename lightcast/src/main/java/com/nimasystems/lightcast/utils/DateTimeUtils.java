f196378a6ebc30281a9eda18716959b21eee986f
Martin Kovachev <miracle@nimasystems.com>
2020-08-06 09:11:28 +0300

    Date time utils

--- lightcast/src/main/java/com/nimasystems/lightcast/utils/DateTimeUtils.java
+++ lightcast/src/main/java/com/nimasystems/lightcast/utils/DateTimeUtils.java
@@ -29,6 +29,15 @@ public class DateTimeUtils {
         return dateToString(date, FORMAT_ISO_DATE);
     }
 
+    public static boolean isSameDay(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal2.setTime(date2);
+        return cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
+                cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR);
+    }
+
     public static String dateToHourMinutesStringFormat(Date date) {
         return dateToString(date, FORMAT_SHORT_TIME_TIME);
     }
